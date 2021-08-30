07eba5b2229354a36cd57c71dc0322bb294748b6
XSectorz <81065047+XSectorz@users.noreply.github.com>
2021-08-30 22:05:08 +0700

    [ REMOVE FOR REPLACE ]

--- HelloSFML/MapHandler.cpp
+++
@@ -1,7 +0,0 @@
-#include "MapHandler.h"
-#include "Platform.h"
-
-vector<TilemapHandler> Maps;
-sf::RenderWindow windowRender(sf::VideoMode(1024, 925), "SFML Hello", sf::Style::Close | sf::Style::Resize);
-sf::Texture playerTexture;
-Player player(&playerTexture, sf::Vector2u(3, 8), 0.3f, 100.0f);
